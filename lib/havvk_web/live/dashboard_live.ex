d661adb747887e4dd8aea3d5dc6f01e34f759a18
James <IdiosApps@users.noreply.github.com>
2023-06-26 07:07:19 +0100

    poc: nested table

--- lib/havvk_web/live/dashboard_live.ex
+++ lib/havvk_web/live/dashboard_live.ex
@@ -9,37 +9,52 @@ defmodule HavvkWeb.DashboardLive do
       <h1>Hello, LiveView!</h1>
 
       <p>Current time: <%= @current_time %></p>
-
-      <%!-- <p>Versions: <%= @versions %></p> --%>
-
+       <%!-- <p>Versions: <%= @versions %></p> --%>
       <table>
         <tr>
           <th>Application</th>
+
           <th>dev</th>
+
           <th>qa</th>
+
           <th>prod</th>
         </tr>
-
 
         <%= for {appKey, appDetails} <- @versions do %>
           <tr>
             <td>app: <%= appKey %></td>
-
-            <%!-- works! amazing --%>
-            <td class={@versions["app1"]["dev"]}> <%= appDetails["dev"] %></td>
-
-            <%!-- also works, great --%>
-            <td class={@versions[appKey]["dev"]}> using vars</td>
-
-            <%!-- also works! --%>
-            <td class={appDetails["dev"]}> using no @</td>
+             <%!-- works! amazing --%>
+            <td class={@versions["app1"]["dev"]}><%= appDetails["dev"] %></td>
+             <%!-- also works, great --%>
+            <td class={@versions[appKey]["dev"]}>using vars</td>
+             <%!-- also works! --%>
+            <td class={appDetails["dev"]}>using no @</td>
 
             <td class="bg-amber-500"><%= appDetails["qa"] %></td>
 
             <td><%= appDetails["prod"] %></td>
-            <%!--  --%>
+             <%!--  --%>
           </tr>
-       <% end %>
+        <% end %>
+      </table>
+
+      <table class="table-auto border-collapse border border-green-800 m-4">
+        <%= for _ <- 1..3 do %>  <%!-- 3 rows --%>
+          <tr>
+            <%= for _ <- 1..3 do %>  <%!--  3 cols --%>
+              <td class="border border-green-600 p-2">
+                <table class="table-auto border-collapse border border-blue-500 w-full">
+                 <%= for nested_id <- 1..3 do %>  <%!--  3 rows (1 col) per cell --%>
+                    <tr>
+                        <td class="border border-blue-300 p-1 text-center"><%= nested_id %></td>
+                    </tr>
+                  <% end %>
+                </table>
+              </td>
+            <% end %>
+          </tr>
+        <% end %>
       </table>
     </div>
     """
@@ -53,11 +68,9 @@ defmodule HavvkWeb.DashboardLive do
         versions: DashboardData.get_versions_mocked()
       )
 
-
     IO.inspect(socket)
     IO.puts("checking socket assigns, added versions")
 
     {:ok, socket}
   end
-
 end
